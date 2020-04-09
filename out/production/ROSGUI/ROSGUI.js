fe61260ee55646f1c5a4eee447c3939e9d22d8ea
Jyumpp <hdhdog@live.com>
2020-04-09 15:47:58 -0400

    fixed dependencies

--- out/production/ROSGUI/ROSGUI.js
+++ out/production/ROSGUI/ROSGUI.js
@@ -8,15 +8,11 @@ if (typeof kotlin === 'undefined') {
   var ROS3D$Viewer = ROS3D.Viewer;
   var ROSLIB$TFClient = ROSLIB.TFClient;
   var ROS3D$InteractiveMarkerClient = ROS3D.InteractiveMarkerClient;
-  var Unit = Kotlin.kotlin.Unit;
   function init(args) {
-    var tmp$, tmp$_0;
     var ros = new ROSLIB$Ros(json([to('url', 'ws://rosbox.local:9090')]));
     var viewer = new ROS3D$Viewer(json([to('divID', 'markers'), to('width', 400), to('height', 400), to('antialias', true)]));
     var tfClient = new ROSLIB$TFClient(json([to('ros', ros), to('angularThres', 0.01), to('rate', 10.0), to('fixedFrame', '/rotating_frame')]));
     var imClient = new ROS3D$InteractiveMarkerClient(json([to('ros', ros), to('tfClient', tfClient), to('topic', '/basic_controls'), to('camera', viewer.camera), to('rootObject', viewer.selectableObjects)]));
-    (tmp$ = document.getElementById('markers')) != null ? (tmp$.setAttribute('border-style', 'solid'), Unit) : null;
-    (tmp$_0 = document.getElementById('markers')) != null ? (tmp$_0.setAttribute('border-style', 'solid'), Unit) : null;
     return 0;
   }
   _.init_kand9s$ = init;
